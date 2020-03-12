92710f992d516627ca940847b718c246f0cc1f00
MATH RUDN <rudn.math@outlook.com>
2020-03-12 11:15:33 +0300

    Добавьте файлы проекта.

---
+++ UI_testing/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace UI_testing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ButtonTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(346, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ButtonClickMeClick);
+            // 
+            // ButtonTimer
+            // 
+            this.ButtonTimer.Tick += new System.EventHandler(this.ButtonTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer ButtonTimer;
+    }
+}
+
---
+++ UI_testing/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI_testing
+{
+    public partial class Form1 : Form
+    {
+        private int _FrameIndex;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClickMeClick(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Hello World!");
+            //ButtonTimer.Enabled = true;
+            ButtonTimer.Enabled =
+                !ButtonTimer.Enabled;
+
+        }
+
+        private void ButtonTimer_Tick(object sender, EventArgs e)
+        {
+            button1.Left+= 8;
+            if (button1.Left > Width)
+                button1.Left = -99;
+            _FrameIndex++;
+            Text = "Frame #" + _FrameIndex;
+        }
+        
+    }
+}
